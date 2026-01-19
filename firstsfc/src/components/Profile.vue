ebc91a8647bb15b2a8cae42a68b6bdf9aa96b5ef
Gwyneth Salud <gcsalud@student.apc.edu.ph>
2026-01-20 00:28:04 +0800

    Typo fix

--- firstsfc/src/components/Profile.vue
+++ firstsfc/src/components/Profile.vue
@@ -861,7 +861,7 @@ export default {
 <style>
 @import url('https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css');
 @import url('https://fonts.googleapis.com/css2?family=Press+Start+2P&display=swap');
-@import "./css/PersonalProfile.css";
+@import "./css/style.css";
 
 /* Add your .fade-enter-active transitions here too! */
 </style>
