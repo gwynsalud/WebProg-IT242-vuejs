50fd078488139e1cd083675dac2fc4694653d000
Gwyneth Salud <gcsalud@student.apc.edu.ph>
2026-01-19 23:54:55 +0800

    Fixing sources

--- firstsfc/src/App.vue
+++ firstsfc/src/App.vue
@@ -7,3 +7,12 @@
 
 <script></script>
 
+<style>
+@import url('https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css');
+@import url('https://fonts.googleapis.com/css2?family=Press+Start+2P&display=swap');
+@import "./css/PersonalProfile.css";
+
+.pixel-text {
+  font-family: 'Press Start 2P', cursive;
+}
+</style>
--- firstsfc/src/components/Profile.vue
+++ firstsfc/src/components/Profile.vue
@@ -1,21 +1,5 @@
 <template> 
 
-<head>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-
-  <title>Gwen's Profile</title>
-  
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
-
-  <link rel="preconnect" href="https://fonts.googleapis.com">
-  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-  <link href="https://fonts.googleapis.com/css2?family=Press+Start+2P&display=swap" rel="stylesheet">
-
-  <!-- Custom CSS -->
-  <link rel="stylesheet" href="css/style.css">
-</head>
-
 <body>
   <div id="guestbook-app" v-cloak>
   <button class="pause-btn" v-on:click="togglePause">
