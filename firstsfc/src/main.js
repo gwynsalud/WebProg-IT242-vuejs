35dde86c200a959a642a3d2dd0240ede6b2ea7a3
Gwyneth Salud <gcsalud@student.apc.edu.ph>
2026-01-27 10:51:24 +0800

    Added comments and comment form

--- firstsfc/src/main.js
+++ firstsfc/src/main.js
@@ -4,9 +4,14 @@ import App from './App.vue'
 import FoodItem from './components/FoodItem.vue'
 import FoodItem2 from './components/FoodItem2.vue'
 import Profile from './components/Profile.vue'
+import CommentForm from './components/CommentForm.vue'
+import Comment from './components/Comment.vue'
+
 
 const app = createApp(App)
 app.component('food-item', FoodItem)
 app.component('food-item2', FoodItem2)
 app.component('my-profile', Profile)
+app.component('comment-form', CommentForm)
+app.component('comment', Comment)
 app.mount('#app')
